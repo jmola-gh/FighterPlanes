5cef49baf3b4710bcb9ced69e6539e3e67343590
jmola-gh <jacobmola789@gmail.com>
2024-11-03 18:08:31 -0500

    Added elite enemy
    - Added elite enemy script + prefab to scene
    - Edited GameManager script + gameobj

--- Fighter Planes F24/Assets/Scripts/GameManager.cs
+++ Fighter Planes F24/Assets/Scripts/GameManager.cs
@@ -7,12 +7,14 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
+    public GameObject enemyElite;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemyElite", 3f, 5f);
     }
 
     // Update is called once per frame
@@ -24,5 +26,10 @@ public class GameManager : MonoBehaviour
     void CreateEnemy()
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 8f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyElite()
+    {
+        Instantiate(enemyElite, new Vector3(-12, Random.Range(2f, 7f), 0), Quaternion.identity);
     }
 }
